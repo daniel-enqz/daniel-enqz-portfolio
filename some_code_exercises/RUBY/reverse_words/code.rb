e4cdd047c8d4b1c4b8e02f93f4050da5e3dcb70c
Daniel Enriquez <daniel.enm17@gmail.com>
2022-04-18 10:19:51 -0500

    file created

---
+++ some_code_exercises/RUBY/reverse_words/code.rb
@@ -0,0 +1,8 @@
+# Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (like the title of this question).
+# Spaces will be included only when more than one word is present.
+
+def spin_words(sentece)
+
+end
+
+spin_words("Hey fellow Le Wagon alumni")
