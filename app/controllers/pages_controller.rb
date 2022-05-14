0c173dab063736b59e607daa3d86986085932188
Daniel Enriquez <daniel.enm17@gmail.com>
2022-05-13 22:14:21 -0500

    pages added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def projects
+  end
+
+  def stack
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get 'home', to: 'pages#home'
   root "pages#home"
+  get 'home', to: 'pages#home'
+  get 'projects', to: 'pages#projects'
+  get 'about', to: 'pages#about'
+  get 'stack', to: 'pages#stack'
 end
