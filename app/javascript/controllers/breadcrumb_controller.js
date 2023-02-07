789f1a731f19bb8174d62825bc96756c3fb90c69
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-07 00:23:09 -0600

    breadcrumb completed

--- app/javascript/controllers/breadcrumb_controller.js
+++ app/javascript/controllers/breadcrumb_controller.js
@@ -2,7 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-
+  static targets = [ "dropdown", "dropdownButton" ]
   connect() {
+  }
+
+  toggleDropdown() {
+    this.dropdownTarget.classList.toggle("hidden")
+    this.dropdownButtonTarget.innerHTML = this.dropdownTarget.classList.contains("hidden") ? '<i class="fa-solid fa-bars"></i>' : '<i class="fa-solid fa-bars-staggered"></i>'
+    this.dropdownButtonTarget.classList.toggle("animate-bounce")
   }
 }
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Raises error for missing translations.
+  config.assets.debug = true
+
+  # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
