95228cb85ffc5e0cb132044bf6b9e995ff586272
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-07 02:42:44 -0600

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_065916) do
   enable_extension "plpgsql"
 
   create_table "requests", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
     t.string "subject"
     t.text "message"
@@ -27,7 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_065916) do
     t.text "message"
     t.integer "rating"
     t.text "email"
-    t.text "name"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
