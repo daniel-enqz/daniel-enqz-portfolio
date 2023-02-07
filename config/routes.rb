09ff04302e72daf9f267708354c1825f2a9afae6
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-07 00:41:47 -0600

    form completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
-  get 'home', to: 'pages#home'
+  get "home", to: "pages#home"
+
+  # Defines the reviews path route ("/reviews")
+  resources :reviews, only: [:create]
 end
