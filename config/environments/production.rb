3456ec4eed26992a6fb5b91ffbca670f884e1cd4
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-08 23:25:51 -0600

    mailer options sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {address:              "smtp.sendgrid.net",
-                                        port:                 465,
+                                        port:                 587,
                                         user_name:            ENV["SENDGRID_USERNAME"],
                                         password:             ENV["SENDGRID_PASSWORD"],
                                         authentication:       "plain",
