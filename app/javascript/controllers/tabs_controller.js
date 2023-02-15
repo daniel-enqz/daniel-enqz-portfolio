db155f9d43f5d92df78cfb68d69d77e9c7fdd42c
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-15 13:29:45 -0600

    first approach with tabs

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -2,28 +2,38 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = [ "requestTab", "reviewTab", "requestButton", "reviewButton"]
+  static targets = [ "requestTab", "reviewTab", "tabButton" ]
+  static classes = {
+    reviewButton: "item-animate border-indigo-500 text-indigo-600",
+    requestButton: "item-animate border-pink-500 text-pink-600",
+    inactiveButton: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+  }
 
   connect() {
   }
 
-  toggleRequestTab() {
-    this.requestTabTarget.classList.remove("hidden")
-    this.reviewTabTarget.classList.add("hidden")
-    this.requestTabTarget.classList.add("item-animate")
-    this.requestButtonTarget.classList.add("border-pink-500", "text-pink-600")
-    this.requestButtonTarget.classList.remove("border-transparent", "text-gray-500", "hover:text-gray-700", "hover:border-gray-300")
-    this.reviewButtonTarget.classList.add("border-transparent", "text-gray-500", "hover:text-gray-700", "hover:border-gray-300")
-    this.reviewButtonTarget.classList.remove("border-indigo-500", "text-indigo-600")
+  toggleTab(button) {
+    const identifier = button.target.id
+    this.setTab(this[`${identifier}TabTarget`], button.target, identifier)
   }
 
-  toggleReviewTab() {
-    this.requestTabTarget.classList.add("hidden")
-    this.reviewTabTarget.classList.remove("hidden")
-    this.reviewTabTarget.classList.add("item-animate")
-    this.reviewButtonTarget.classList.add("border-indigo-500", "text-indigo-600")
-    this.reviewButtonTarget.classList.remove("border-transparent", "text-gray-500", "hover:text-gray-700", "hover:border-gray-300")
-    this.requestButtonTarget.classList.add("border-transparent", "text-gray-500", "hover:text-gray-700", "hover:border-gray-300")
-    this.requestButtonTarget.classList.remove("border-pink-500", "text-pink-600")
+  hideAllOtherTabs(identifier) {
+    this.tabButtonTargets.forEach((tb) => {
+      if (tb.id != identifier) {
+        tb.classList.add(this.classes.inactiveButton)
+        tb.classList.remove(this.classes[`${tb.id}Button`])
+      }
+    })
+    this.targets.forEach((t) => {
+      if (t != this[`${identifier}TabTarget`]) {
+        t.classList.add("hidden")
+      }
+    })
+  }
+
+  setTab(tab, button, identifier) {
+    tab.classList.remove("hidden")
+    button.classList.remove(this.classes.inactiveButton)
+    button.classList.add(this.classes[`${identifier}Button`])
   }
 }
