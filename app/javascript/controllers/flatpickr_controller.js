92f5bcfa689b0dd26d4bbe4a13af3241d676c206
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-19 21:08:57 -0600

    remove controllers

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -13,5 +13,5 @@ export default class extends Flatpickr {
     };
 
     super.connect();
-   }
+  }
 }
--- db/schema.rb
+++ db/schema.rb
@@ -34,5 +34,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_023544) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
