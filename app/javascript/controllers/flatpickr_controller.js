de9a3f9f514f0ff48fdc98739db88c5c76461990
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-19 22:46:06 -0600

    hide input

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -5,8 +5,11 @@ export default class extends Flatpickr {
   initialize() {
     //global options
      this.config = {
+      altInput: true,
+      altFormat: "F j at h:i K",
+      altInputClass: "invisible",
       enableTime: true,
-      dateFormat: "F j at h:i K",
+      dateFormat: "F j, Y",
       inline: true,
       minDate: "today"
      };
