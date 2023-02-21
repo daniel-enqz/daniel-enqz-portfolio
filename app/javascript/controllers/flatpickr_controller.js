c3e0cb40d27f7a67eacc788c8a93a88381390c57
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-21 12:46:58 -0600

    Disablek mobile flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -5,11 +5,12 @@ export default class extends Flatpickr {
   initialize() {
      this.config = {
       enableTime: true,
-      dateFormat: "F j, Y at H:i",
+      dateFormat: "F j, Y at H:i K",
       inline: true,
       minDate: "today",
       defaultHour: 9,
       defaultMinute: 30,
+      disableMobile: "true"
      };
    }
 }
