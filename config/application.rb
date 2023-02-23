c32370b695e3b71e0adb2f273791283fcc8c5953
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-22 23:38:31 -0600

    small changes in app.rb

--- config/application.rb
+++ config/application.rb
@@ -9,14 +9,8 @@ Bundler.require(*Rails.groups)
 module DanielEnqz
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join("app/components")
-
     config.load_defaults 7.0
     config.exceptions_app = routes
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
     config.time_zone = "Mexico City"
     # config.eager_load_paths << Rails.root.join("extras")
   end
