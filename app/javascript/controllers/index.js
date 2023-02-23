66197d18a1428517c5db3d7eababea7636584140
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-23 14:50:50 -0600

    remove all flatpickr instances

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,10 +6,6 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-// Using extended version so no longer need default flatpickr controller:
-// import Flatpickr from 'stimulus-flatpickr'
-// application.register('flatpickr', Flatpickr)
-
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
